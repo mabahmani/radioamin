338f4d3488f5a1e7be74410e9addae891cff1008
MaB <aminbahmani10@gmail.com>
2020-03-19 23:16:02 +0330

    add searchable fields in django admin

--- musicApp/admin.py
+++ musicApp/admin.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 
 from musicApp.models import Music, Singer, Genre, Album, Playlist
 
-admin.site.register(Music)
-admin.site.register(Singer)
+
+class MusicAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    filter_horizontal = ('genre', )
+    autocomplete_fields = ('singer', )
+
+
+class SingerAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+admin.site.register(Music, MusicAdmin)
+admin.site.register(Singer, SingerAdmin)
 admin.site.register(Genre)
 admin.site.register(Album)
 admin.site.register(Playlist)
