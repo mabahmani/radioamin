db45801e71357734bfdf50450e25e4790b6befbc
MaB <aminbahmani10@gmail.com>
2020-04-06 16:27:53 +0430

    start register ajax imp

--- account/views.py
+++ account/views.py
@@ -1,9 +1,21 @@
-from django.http import HttpResponse
+import json
+
+from django.contrib.auth.models import User
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 
 def sign_up(request):
     email = request.POST.get('email', None)
     password = request.POST.get('password', None)
+    response = {}
 
-    return HttpResponse(email + password)
+    if User.objects.filter(username=email).exists():
+        response['status'] = 'error'
+        response['msg'] = 'user already exist.'
+        return JsonResponse(json.dumps(response), safe=False)
+    else:
+        User.objects.create_user(email, email, password)
+        response['status'] = 'ok'
+        response['msg'] = 'user created'
+        return JsonResponse(json.dumps(response), safe=False)
