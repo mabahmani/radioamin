d8eb584e7f8581a4d905151e1d4e23bcacb97703
MaB <aminbahmani10@gmail.com>
2020-04-10 16:41:05 +0430

    change urls

--- radioamin/urls.py
+++ radioamin/urls.py
@@ -26,6 +26,8 @@ urlpatterns = [
     path('', include('musicApp.urls')),
     path('account/', include('account.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL,
+                           document_root=settings.MEDIA_ROOT)
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL,
 #                           document_root=settings.MEDIA_ROOT)
